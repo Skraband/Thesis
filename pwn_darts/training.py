12220e872dff86892d0e4d09d8a1210fb0a1d55d
Skraband <fabian.kalter@gmx.de>
2023-07-08 13:34:28 +0200

    256 batch size M4 arch

--- pwn_darts/training.py
+++ pwn_darts/training.py
@@ -169,7 +169,7 @@ for i, (data_source, preprocessing, model, evaluation_metrics, plots, load, use_
 
     if load is None:
         print('Training model...')
-        model.train(train_x, train_y, test_x, test_y, embedding_sizes,batch_size=128, epochs=15000)
+        model.train(train_x, train_y, test_x, test_y, embedding_sizes,batch_size=256, epochs=15000)
         print(f'--- Training finished after {(datetime.now() - start_time).microseconds} microseconds ---')
 
     else:
